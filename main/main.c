882c2479caf847e47a778a2c54decf931c2de149
Mateusz Szpot <wth.msz777@proton.me>
2024-12-21 14:36:38 +0100

    Macros library and todo update

--- main/main.c
+++ main/main.c
@@ -2,17 +2,8 @@
 #include "MKL05Z4.h"
 #include "i2c.h"
 #include "frdm_bsp.h"
+#include "macroBlock.h"
 
-#define TX 1
-#define RX 2
-
-// accelerometer registers
-#define ADDRESS 0x1d
-#define XYZ_DATA_CFG_REG 0x0e
-#define CTRL_REG1 0x2a
-#define STATUS 0x00
-#define ACC_DATA_REGS 0x01
-#define HP_FILTER_CUTOFF 0x0f
 
 void setup();
 
