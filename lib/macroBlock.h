71eb6dd7805046593e01efa473cf4c07ddaa2a9b
Mateusz Szpot <wth.msz777@proton.me>
2025-01-16 19:25:59 +0100

    move accelerometer adresses to MMA.c

--- lib/macroBlock.h
+++ lib/macroBlock.h
@@ -2,13 +2,6 @@
 #define TX 1
 #define RX 2
 
-// accelerometer registers
-#define ADDRESS 0x1d
-#define XYZ_DATA_CFG_REG 0x0e
-#define CTRL_REG1 0x2a
-#define STATUS_REG 0x00
-#define ACC_DATA_REGS 0x01
-#define HP_FILTER_CUTOFF 0x0f
 
 // tresholds
 #define ACTIVATION_TH -0.15
