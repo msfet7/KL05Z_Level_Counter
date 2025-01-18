06141b29f9ef2ddf5af37a37ce5fad1150d5540d
Mateusz Szpot <wth.msz777@proton.me>
2025-01-18 16:19:07 +0100

    Adding hex value of activation treshold and also debounce counter counts values for acc registers

--- lib/macroBlock.h
+++ lib/macroBlock.h
@@ -5,10 +5,14 @@
 
 // tresholds
 #define ACTIVATION_TH -0.15
+#define ACTIVATION_TH_HEX 0x03
 #define DEXIT_TH 0.03
 #define UEXIT_TH 0.10
 #define ABSOLUTE_MAXIMUM_TH 0.4
 #define ABSOLUTE_MINIMUM_TH -0.4
+
+// debounce -> 50/1.25 [ms]
+#define DEBOUNCE_COUNTER_HEX 0x28
 
 // for future (i guess)
 #define UNDER_TEST 0
