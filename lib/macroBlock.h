811f023d245ee7298bc2b9c6027083a39d3e6b02
Mateusz Szpot <wth.msz777@proton.me>
2025-01-22 18:06:01 +0100

    corrections to interrupt based code, making code based on <<fused state machine>> instead of if statements

--- lib/macroBlock.h
+++ lib/macroBlock.h
@@ -7,14 +7,14 @@
 
 // tresholds
 #define ACTIVATION_TH -0.15
-#define ACTIVATION_TH_HEX 0x03
+#define ACTIVATION_TH_HEX 0x05
 #define DEXIT_TH 0.03
 #define UEXIT_TH 0.10
 #define ABSOLUTE_MAXIMUM_TH 0.4
 #define ABSOLUTE_MINIMUM_TH -0.4
 
 // debounce -> 50/1.25 [ms]
-#define DEBOUNCE_COUNTER_HEX 0x28
+#define DEBOUNCE_COUNTER_HEX 0x14
 
 // for future (i guess)
 #define UNDER_TEST 0
