eeb3d912f0f342a64e93ba9c001745e866853000
Mateusz <wth.msz777@proton.me>
2025-01-23 17:16:52 +0100

    small treshold update

--- lib/macroBlock.h
+++ lib/macroBlock.h
@@ -7,14 +7,14 @@
 
 // tresholds
 #define ACTIVATION_TH -0.15
-#define ACTIVATION_TH_HEX 0x05
+#define ACTIVATION_TH_HEX 0x04
 #define DEXIT_TH 0.03
-#define UEXIT_TH 0.10
+#define UEXIT_TH 0.05
 #define ABSOLUTE_MAXIMUM_TH 0.4
 #define ABSOLUTE_MINIMUM_TH -0.4
 
 // debounce -> 50/1.25 [ms]
-#define DEBOUNCE_COUNTER_HEX 0x14
+#define DEBOUNCE_COUNTER_HEX 0x03
 
 // for future (i guess)
 #define UNDER_TEST 0
