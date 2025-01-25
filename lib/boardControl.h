1408c43354ab5c3bd612ed5bf4114d37f49c0f46
Mateusz Szpot <wth.msz777@proton.me>
2025-01-25 15:09:16 +0100

    adding library protection and functions for debug enable

--- lib/boardControl.h
+++ lib/boardControl.h
@@ -1,17 +1,28 @@
+#ifndef BOARD_CONTROL
+#define BOARD_CONTROL
+
 #include <stdio.h>
 #include "MKL05Z4.h"
 #include "macroBlock.h"
-#include "i2c.h"
 #include "MMA.h"
+#include "gui.h"
 
+
+//void debug();
 // function in loop
 void execute();
 
 // function in setup
 void setup();
 
+void debugControl();
+
+//void debugState();
+
 // MMA interrupt handler
 void MMAIntControl();
 
 // PIT interrupt handler
 void PITIntControl();
+
+#endif //BOARD_CONTROL
